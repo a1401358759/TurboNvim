523a99e0cd3d13827023f3b47e891143d263006b
yxuefeng <yxuefeng@infervision.com>
2023-12-11 19:07:26 +0800

    add satellite plugin for scrollbar

---
+++ lua/plugins/satellite.lua
@@ -0,0 +1,43 @@
+-- -@diagnostic disable: missing-fields
+return {
+  "lewis6991/satellite.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("satellite").setup({
+      current_only = true,
+      winblend = 50,
+      zindex = 40,
+      excluded_filetypes = { "neo-tree", "nvim-tree", "alpha", "dashboard" },
+      width = 2,
+      handlers = {
+        cursor = {
+          enable = false,
+          symbols = { "⎺", "⎻", "⎼", "⎽" },
+        },
+        search = {
+          enable = true,
+        },
+        diagnostic = {
+          enable = true,
+          signs = { "-", "=", "≡" },
+          min_severity = vim.diagnostic.severity.HINT,
+        },
+        gitsigns = {
+          enable = false,
+        },
+        marks = {
+          enable = true,
+          show_builtins = false, -- shows the builtin marks like [ ] < >
+          key = "m",
+          -- Highlights:
+          -- SatelliteMark (default links to Normal)
+        },
+        quickfix = {
+          signs = { "-", "=", "≡" },
+          -- Highlights:
+          -- SatelliteQuickfix (default links to WarningMsg)
+        },
+      },
+    })
+  end,
+}
