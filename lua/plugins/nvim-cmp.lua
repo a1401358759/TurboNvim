55a4eb5c9ad62773fcb6895dabb7024c992bcd63
yxuefeng <yxuefeng@infervision.com>
2024-01-06 22:29:31 +0800

    fix bug

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,44 +1,45 @@
 ---@diagnostic disable: missing-fields
 local setCompHL = function()
+  local hl = vim.api.nvim_set_hl
   local fgdark = "#2E3440"
-  vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
-  vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
-  vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
-  vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#808080", bg = "NONE", italic = true })
-  vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = fgdark, bg = "#B5585F" })
-  vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = fgdark, bg = "#B5585F" })
-  vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = fgdark, bg = "#B5585F" })
-  vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = fgdark, bg = "#9FBD73" })
-  vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = fgdark, bg = "#9FBD73" })
-  vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = fgdark, bg = "#9FBD73" })
-  vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = fgdark, bg = "#D4BB6C" })
-  vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = fgdark, bg = "#D4BB6C" })
-  vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = fgdark, bg = "#D4BB6C" })
-  vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = fgdark, bg = "#A377BF" })
-  vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = fgdark, bg = "#A377BF" })
-  vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = fgdark, bg = "#A377BF" })
-  vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = fgdark, bg = "#A377BF" })
-  vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = fgdark, bg = "#A377BF" })
-  vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = fgdark, bg = "#7E8294" })
-  vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = fgdark, bg = "#7E8294" })
-  vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = fgdark, bg = "#D4A959" })
-  vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = fgdark, bg = "#D4A959" })
-  vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = fgdark, bg = "#D4A959" })
-  vim.api.nvim_set_hl(0, "CmpItemKindTabNine", { fg = fgdark, bg = "#D4A959" })
-  vim.api.nvim_set_hl(0, "CmpItemKindCodeium", { fg = fgdark, bg = "#D4A959" })
-  vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = fgdark, bg = "#6C8ED4" })
-  vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = fgdark, bg = "#6C8ED4" })
-  vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = fgdark, bg = "#6C8ED4" })
-  vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = fgdark, bg = "#58B5A8" })
-  vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = fgdark, bg = "#58B5A8" })
-  vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = fgdark, bg = "#58B5A8" })
-  vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+  hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+  hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+  hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+  hl(0, "CmpItemMenu", { fg = "#808080", bg = "NONE", italic = true })
+  hl(0, "CmpItemKindField", { fg = fgdark, bg = "#B5585F" })
+  hl(0, "CmpItemKindProperty", { fg = fgdark, bg = "#B5585F" })
+  hl(0, "CmpItemKindEvent", { fg = fgdark, bg = "#B5585F" })
+  hl(0, "CmpItemKindText", { fg = fgdark, bg = "#9FBD73" })
+  hl(0, "CmpItemKindEnum", { fg = fgdark, bg = "#9FBD73" })
+  hl(0, "CmpItemKindKeyword", { fg = fgdark, bg = "#9FBD73" })
+  hl(0, "CmpItemKindConstant", { fg = fgdark, bg = "#D4BB6C" })
+  hl(0, "CmpItemKindConstructor", { fg = fgdark, bg = "#D4BB6C" })
+  hl(0, "CmpItemKindReference", { fg = fgdark, bg = "#D4BB6C" })
+  hl(0, "CmpItemKindFunction", { fg = fgdark, bg = "#A377BF" })
+  hl(0, "CmpItemKindStruct", { fg = fgdark, bg = "#A377BF" })
+  hl(0, "CmpItemKindClass", { fg = fgdark, bg = "#A377BF" })
+  hl(0, "CmpItemKindModule", { fg = fgdark, bg = "#A377BF" })
+  hl(0, "CmpItemKindOperator", { fg = fgdark, bg = "#A377BF" })
+  hl(0, "CmpItemKindVariable", { fg = fgdark, bg = "#7E8294" })
+  hl(0, "CmpItemKindFile", { fg = fgdark, bg = "#7E8294" })
+  hl(0, "CmpItemKindUnit", { fg = fgdark, bg = "#D4A959" })
+  hl(0, "CmpItemKindSnippet", { fg = fgdark, bg = "#D4A959" })
+  hl(0, "CmpItemKindFolder", { fg = fgdark, bg = "#D4A959" })
+  hl(0, "CmpItemKindTabNine", { fg = fgdark, bg = "#D4A959" })
+  hl(0, "CmpItemKindCodeium", { fg = fgdark, bg = "#D4A959" })
+  hl(0, "CmpItemKindMethod", { fg = fgdark, bg = "#6C8ED4" })
+  hl(0, "CmpItemKindValue", { fg = fgdark, bg = "#6C8ED4" })
+  hl(0, "CmpItemKindEnumMember", { fg = fgdark, bg = "#6C8ED4" })
+  hl(0, "CmpItemKindInterface", { fg = fgdark, bg = "#58B5A8" })
+  hl(0, "CmpItemKindColor", { fg = fgdark, bg = "#58B5A8" })
+  hl(0, "CmpItemKindTypeParameter", { fg = fgdark, bg = "#58B5A8" })
+  hl(0, "CmpGhostText", { link = "Comment", default = true })
 end
 
 return {
   "hrsh7th/nvim-cmp",
   version = false, -- last release is way too old
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
@@ -49,7 +50,6 @@ return {
     "hrsh7th/cmp-emoji",
     "saadparwaiz1/cmp_luasnip",
     "friendly-snippets",
-    "tzachar/cmp-tabnine",
     "lukas-reineke/cmp-rg",
     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
   },
@@ -62,7 +62,31 @@ return {
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
-
+    local comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    }
+    local sources = {
+      { name = "nvim_lsp" },
+      { name = "buffer" },
+      { name = "rg", keyword_length = 3 },
+      { name = "luasnip" },
+      { name = "vsnip" },
+      { name = "path" },
+      { name = "emoji" },
+    }
+    local options = require("config.options")
+    if options.use_ai_plugins then
+      table.insert(comparators, 0, require("cmp_tabnine.compare"))
+      table.insert(sources, 3, { name = "codeium" })
+      table.insert(sources, 4, { name = "cmp_tabnine" })
+    end
     cmp.setup({
       -- Insert or Replace
       confirmation = {
@@ -126,30 +150,8 @@ return {
         end, { "i", "s" }),
       }),
       -- define sorting rules
-      sorting = {
-        comparators = {
-          require("cmp_tabnine.compare"),
-          cmp.config.compare.offset,
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "codeium" },
-        { name = "cmp_tabnine" },
-        { name = "buffer" },
-        { name = "rg", keyword_length = 3 },
-        { name = "luasnip" },
-        { name = "vsnip" },
-        { name = "path" },
-        { name = "emoji" },
-      }),
+      sorting = { comparators = comparators },
+      sources = cmp.config.sources(sources),
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, item)
@@ -179,11 +181,18 @@ return {
 
     local cmp_mapping = {
       ["<Tab>"] = {
-        c = function()
+        c = function(_)
           if cmp.visible() then
-            cmp.select_next_item()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
           else
             cmp.complete()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            end
           end
         end,
       },
@@ -217,38 +226,16 @@ return {
       ["<C-e>"] = {
         c = cmp.mapping.abort(),
       },
-      ["<CR>"] = {
-        c = cmp.mapping.confirm({ select = false }),
-      },
     }
-
-    -- Set configuration for specific filetype.
-    cmp.setup.filetype("gitcommit", {
-      sources = cmp.config.sources({
-        { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-      }, {
-        { name = "buffer" },
-      }),
-    })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp_mapping,
       sources = {
         { name = "buffer" },
-        { name = "rg" },
       },
     })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(":", {
-      mapping = cmp_mapping,
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-    })
     setCompHL()
   end,
 }
