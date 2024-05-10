35f3df9211b4029f9eace02e624f986c6cfddce7
yxuefeng <yxuefeng@infervision.com>
2024-05-10 10:13:48 +0800

    use ruff instead of ruff_lsp because of ruff is fully supported

--- lua/lspservers/ruff.lua
+++ lua/lspservers/ruff.lua
@@ -1,11 +1,13 @@
 -- Done
 
 return {
-  enabled = false,
-  filetypes = { "python" },
-  single_file_support = true,
-  cmd = { "ruff", "server", "--preview" },
-  settings = {
-    configuration = "~/.config/nvim/pyproject.toml",
+  enabled = true,
+  init_options = {
+    filetypes = { "python" },
+    single_file_support = true,
+    cmd = { "ruff", "server", "--preview" },
+    settings = {
+      configuration = "~/.config/nvim/pyproject.toml",
+    },
   },
 }
