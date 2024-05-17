7020861f44b5d95455fb4d68d68993bd1d5731f4
yxuefeng <yxuefeng@infervision.com>
2024-05-17 18:59:38 +0800

    Sync LazyVim updates, huge 0.10 stable, add mini.files plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,45 +33,28 @@ local setCompHL = function()
   hl(0, "CmpItemKindInterface", { fg = fgdark, bg = "#58B5A8" })
   hl(0, "CmpItemKindColor", { fg = fgdark, bg = "#58B5A8" })
   hl(0, "CmpItemKindTypeParameter", { fg = fgdark, bg = "#58B5A8" })
-  hl(0, "CmpGhostText", { link = "Comment", default = true })
 end
 
 return {
   "hrsh7th/nvim-cmp",
+  version = false, -- last release is way too old
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "lukas-reineke/cmp-rg",
     {
-      "L3MON4D3/LuaSnip",
-      dependencies = {
-        {
-          "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-          end,
-        },
-      },
-      opts = { history = true, delete_check_events = "TextChanged" },
-      config = function(_, opts)
-        require("luasnip").config.set_config(opts)
-      end,
-    },
-    {
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "lukas-reineke/cmp-rg",
+      { "rafamadriz/friendly-snippets" },
+      { "garymjr/nvim-snippets", opts = { friendly_snippets = true } },
     },
   },
-  config = function()
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local function has_words_before()
-      local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-    cmp.setup({
+
+    return {
       -- Insert or Replace
       confirmation = {
         default_behavior = cmp.ConfirmBehavior.Insert,
@@ -81,7 +64,7 @@ return {
       },
       snippet = {
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          vim.snippet.expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -92,23 +75,23 @@ return {
           select = true,
         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif cmp.visible() then
             cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          elseif cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
           else
             fallback()
           end
@@ -116,21 +99,19 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "buffer" },
         { name = "rg", keyword_length = 3, max_item_count = 5 },
-        { name = "luasnip" },
         { name = "fittencode" },
         { name = "codeium" },
         { name = "path" },
+        { name = "snippets" },
+      }, {
+        { name = "buffer" },
       }),
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, item)
           local icons = require("config.icons").icons.lspkind
           local source = entry.source.name
-          if source == "cmp_tabnine" then
-            source = "tabnine"
-          end
           item.kind = string.format(" %s", icons[item.kind], item.kind)
           item.menu = string.format(" (%s)", string.upper(source))
           return item
@@ -148,7 +129,11 @@ return {
         }),
         documentation = cmp.config.window.bordered(),
       },
-    })
+    }
+  end,
+  config = function(_, opts)
+    local cmp = require("cmp")
+    cmp.setup(opts)
 
     local cmd_mapping = {
       ["<Down>"] = {
@@ -189,7 +174,6 @@ return {
       }, {
         { name = "cmdline" },
       }),
-      matching = { disallow_symbol_nonprefix_matching = false },
       completion = {
         completeopt = "menu,menuone,noselect",
       },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,39 @@
 return {
   {
     "echasnovski/mini.comment",
+    enabled = vim.fn.has("nvim-0.10") == 0,
     event = { "TurboLoad" },
     lazy = true,
-    opts = {},
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+    init = function()
+      if vim.fn.has("nvim-0.10") == 1 then
+        local inject = require("utils.inject")
+        vim.g.skip_ts_context_commentstring_module = true
+        -- Majestically override the native `get_commentstring` function.
+        vim.schedule(function()
+          inject.set_upvalue(
+            inject.get_upvalue(require("vim._comment").textobject, "get_comment_parts"),
+            "get_commentstring",
+            function()
+              return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+            end
+          )
+        end)
+      end
+    end,
   },
   {
     "echasnovski/mini.surround",
@@ -87,6 +117,31 @@ return {
       { "<A-l>", "<cmd>lua MiniMove.move_line('right')<CR>", desc = "Move line right", mode = "n" },
       { "<A-j>", "<cmd>lua MiniMove.move_line('down')<CR>", desc = "Move line down", mode = "n" },
       { "<A-k>", "<cmd>lua MiniMove.move_line('up')<CR>", desc = "Move line up", mode = "n" },
+    },
+  },
+  {
+    "echasnovski/mini.files",
+    version = false,
+    config = function()
+      require("mini.files").setup({
+        mappings = {
+          close = "<Tab>",
+          go_in = "l",
+          go_in_plus = "L",
+          go_out = "h",
+          go_out_plus = "H",
+          reset = "<BS>",
+          reveal_cwd = "<A-Up>",
+          show_help = "g?",
+          synchronize = "=",
+        },
+        windows = {
+          preview = true,
+        },
+      })
+    end,
+    keys = {
+      { "<Tab>", "<cmd>lua MiniFiles.open()<CR>", desc = "Open Mini Files", mode = "n" },
     },
   },
 }
