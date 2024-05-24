f875b3030fd7de51df3aea90f6b605a554e3de03
yxuefeng <yxuefeng@infervision.com>
2024-05-24 09:59:36 +0800

    use ruff_fix and ruff_organize_imports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -41,11 +41,11 @@ return {
         html = { { "prettier", "prettierd" } },
         json = { { "jq", "fixjson", "prettier", "prettierd" } },
         yaml = { { "prettier", "prettierd" } },
-        markdown = { { "prettier", "prettierd" } },
+        markdown = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
         graphql = { { "prettier", "prettierd" } },
         python = function(bufnr)
           if require("conform").get_formatter_info("ruff_format", bufnr).available then
-            return { "isort", "ruff_format" }
+            return { "ruff_fix", "ruff_organize_imports", "ruff_format" }
           else
             return { "isort", "black" }
           end
