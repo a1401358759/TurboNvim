174db4502686ab6483e11ae1ec744841986e07e8
yxuefeng <yxuefeng@infervision.com>
2024-06-05 15:30:30 +0800

    update eslint_d lint config

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,12 +4,8 @@ return {
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        -- Library items can be absolute paths
-        -- "~/projects/my-awesome-lib",
-        -- Or relative, which means they will be resolved as a plugin
-        -- "LazyVim",
-        -- When relative, you can also provide a path to the library in the plugin dir
-        "luvit-meta/library", -- see below
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
       },
     },
   },
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -14,8 +14,24 @@ return {
       python = { "ruff" },
       dockerfile = { "hadolint" },
     }
+
+    -- ruff
     local ruff = require("lint").linters.ruff
     ruff.args = options.ruff_args
+
+    -- eslint_d
+    local eslint_d = require("lint").linters.eslint_d
+    eslint_d.args = {
+      "--no-warn-ignored", -- <-- this is the key argument
+      "--format",
+      "json",
+      "--stdin",
+      "--stdin-filename",
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
+    }
+
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       callback = function()
         require("lint").try_lint()
