8fdf39eed5a0680eacfb9a993cfc3aaf9e25f876
yxuefeng <yxuefeng@infervision.com>
2024-07-22 09:57:18 +0800

    use grug-far.nvim instead of specture.nvim

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,11 +1,25 @@
 return {
-  "nvim-pack/nvim-spectre",
+  "MagicDuck/grug-far.nvim",
   lazy = true,
-  build = false,
-  cmd = "Spectre",
-  opts = { open_cmd = "noswapfile vnew" },
-  -- stylua: ignore
+  opts = { headerMaxWidth = 80 },
+  cmd = "GrugFar",
   keys = {
-    { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    {
+      "<leader>sr",
+      function()
+        local is_visual = vim.fn.mode():lower():find("v")
+        if is_visual then -- needed to make visual selection work
+          vim.cmd([[normal! v]])
+        end
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        local filesFilter = ext and ext ~= "" and "*." .. ext or nil;
+        (is_visual and grug.with_visual_selection or grug.grug_far)({
+          prefills = { filesFilter = filesFilter },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
   },
 }
