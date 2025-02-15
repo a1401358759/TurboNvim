2be496e6b9d17568df1bf784b49dd76920910797
yangxf <yangxf@cmgos.com>
2025-02-15 20:20:23 +0800

    remove cmp,nvim-jdtls and dap plugin,add nvim-java plugin for java develop

---
+++ lua/plugins/nvim-java.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-java/nvim-java",
+  ft = { "java" },
+  config = function()
+    require("java").setup({})
+    require("lspconfig").jdtls.setup({
+      handlers = {
+        -- By assigning an empty function, you can remove the notifications
+        -- printed to the cmd
+        ["$/progress"] = function(_, result, ctx) end,
+      },
+    })
+  end,
+}
