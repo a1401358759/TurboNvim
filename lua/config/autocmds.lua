921336d8149760f1ee2949ab875682c87a1b22df
yxuefeng <yxuefeng@infervision.com>
2025-04-03 14:25:15 +0800

    disabled builtin auto-completion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -240,12 +240,3 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.opt.tabstop = 4 -- Number of spaces tabs count for
   end,
 })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
