fb1628b6c20b03b7b8f33f28038add80d6f1a3dd
yangxf <yangxf@cmgos.com>
2025-05-10 10:07:35 +0800

    Update rainbow-delimiters config

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,6 +1,7 @@
 return {
   "HiPhish/rainbow-delimiters.nvim",
   lazy = true,
+  submodules = false,
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   config = function()
     local rainbow_delimiters = require("rainbow-delimiters")
