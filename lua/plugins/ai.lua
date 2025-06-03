a9b58117c13468766e7043e5a0771f89fea01e9d
yxuefeng <yxuefeng@infervision.com>
2025-06-03 17:21:27 +0800

    Remove avante.nvim and update codecompanion.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -17,60 +17,6 @@ return {
     end,
   },
   {
-    "yetone/avante.nvim",
-    event = "TurboLoad",
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "deepseek_3",
-      behaviour = {
-        auto_suggestions = false,
-        support_paste_from_clipboard = true,
-        enable_cursor_planning_mode = true,
-      },
-      ollama = {
-        api_key_name = "",
-        endpoint = "http://127.0.0.1:11434",
-        model = "deepseek-r1:8b",
-        options = {
-          num_ctx = 32768,
-          temperature = 0,
-        },
-        stream = true,
-      },
-      vendors = {
-        deepseek_3 = {
-          __inherited_from = "openai",
-          endpoint = "https://api.deepseek.com",
-          -- deepseek-reasoner, deepseek-coder, deepseek-chat
-          model = "deepseek-chat",
-          api_key_name = "DEEPSEEK_API_KEY",
-          max_tokens = 8192,
-        },
-        qianwen = {
-          __inherited_from = "openai",
-          api_key_name = "DASHSCOPE_API_KEY",
-          endpoint = "https://dashscope.aliyuncs.com/compatible-mode/v1",
-          model = "qwen-plus",
-        },
-      },
-      windows = {
-        input = {
-          prefix = " ",
-          height = 8, -- Height of the input window in vertical layout
-        },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  },
-  {
     "olimorris/codecompanion.nvim",
     event = "TurboLoad",
     dependencies = {
@@ -97,11 +43,50 @@ return {
           end,
         },
         strategies = {
-          chat = { adapter = "deepseek" },
+          chat = {
+            adapter = "deepseek",
+            keymaps = {
+              send = {
+                modes = { n = "<C-s>", i = "<C-s>" },
+              },
+              close = {
+                modes = { n = "<C-c>", i = "<C-c>" },
+              },
+            },
+            opts = { completion_provider = "blink" },
+          },
           inline = { adapter = "deepseek" },
           agent = { adapter = "deepseek" },
         },
+        display = {
+          action_palette = {
+            provider = "snacks",
+            opts = { show_default_actions = true },
+          },
+        },
+        opts = { language = "Chinese" },
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              show_result_in_chat = true, -- Show mcp tool results in chat
+              make_vars = true, -- Convert resources to #variables
+              make_slash_commands = true, -- Add prompts as /slash commands
+            },
+          },
+        },
       })
+    end,
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "TurboLoad",
+    build = "npm install -g mcp-hub@latest",
+    config = function()
+      require("mcphub").setup()
     end,
   },
 }
