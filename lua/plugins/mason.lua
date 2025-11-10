f73fea1ade0ff5945d2a3c6a07221d0b4c5ef2e8
yxuefeng <yxuefeng@infervision.com>
2025-11-10 11:03:27 +0800

    Update mson package installed icon

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -44,7 +44,7 @@ return {
       width = 0.8,
       height = 0.8,
       icons = {
-        package_installed = "",
+        package_installed = "",
         package_pending = "",
         package_uninstalled = "",
       },
