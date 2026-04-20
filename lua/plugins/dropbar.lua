9ceefb46a82876d61edeb1b3d61a6b470cd8990a
yxuefeng <yxuefeng@infervision.com>
2026-04-20 11:32:12 +0800

    fix: neo-tree 禁用 dropbar

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -39,8 +39,42 @@ return {
   lazy = true,
   event = { "TurboLoad" },
   cond = is_available(),
-  config = function()
-    require("dropbar").setup({})
+  opts = {
+    bar = {
+      enable = function(buf, win, _)
+        buf = vim._resolve_bufnr(buf)
+        if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+          return false
+        end
+
+        if
+          not vim.api.nvim_buf_is_valid(buf)
+          or not vim.api.nvim_win_is_valid(win)
+          or vim.fn.win_gettype(win) ~= ""
+          or vim.wo[win].winbar ~= ""
+          or vim.bo[buf].ft == "help"
+          or vim.bo[buf].ft == "neo-tree"
+        then
+          return false
+        end
+
+        local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+        if stat and stat.size > 1024 * 1024 then
+          return false
+        end
+
+        return vim.bo[buf].bt == "terminal"
+          or vim.bo[buf].ft == "markdown"
+          or pcall(vim.treesitter.get_parser, buf)
+          or not vim.tbl_isempty(vim.lsp.get_clients({
+            bufnr = buf,
+            method = "textDocument/documentSymbol",
+          }))
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("dropbar").setup(opts)
     remove_dropba_color()
   end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,8 +2,48 @@ local icons = require("config.icons").icons
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
+  },
   lazy = true,
   cmd = "Neotree",
+  ---@module 'neo-tree'
+  ---@type neotree.Config
+  opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    ---@diagnostic disable-next-line: missing-fields
+    source_selector = {
+      winbar = false, -- toggle to show selector on winbar
+      statusline = false,
+    },
+    enable_diagnostics = false,
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    default_component_configs = {
+      ---@diagnostic disable-next-line: missing-fields
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "",
+        folder_empty_open = "",
+        use_filtered_colors = true,
+      },
+      modified = { symbol = "" },
+      git_status = { symbols = icons.git },
+      diagnostics = { symbols = icons.diagnostics },
+    },
+    filesystem = {
+      follow_current_file = { enabled = true },
+      filtered_items = {
+        show_hidden_count = false,
+        hide_by_pattern = { -- uses glob style patterns
+          "__pycache__",
+        },
+      },
+    },
+  },
   keys = {
     {
       "<leader>fe",
@@ -28,113 +68,4 @@ return {
     },
     { "<leader>e", "<leader>fe", desc = "Explorer NeoTree", remap = true },
   },
-  deactivate = function()
-    vim.cmd([[Neotree close]])
-  end,
-  init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-      desc = "Start Neo-tree with directory",
-      once = true,
-      callback = function()
-        if package.loaded["neo-tree"] then
-          return
-        else
-          ---@diagnostic disable-next-line: param-type-mismatch
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == "directory" then
-            require("neo-tree")
-          end
-        end
-      end,
-    })
-  end,
-  opts = {
-    enable_diagnostics = false,
-    enable_git_status = true,
-    popup_border_style = "rounded",
-    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-    source_selector = {
-      winbar = false, -- toggle to show selector on winbar
-      statusline = true,
-      content_layout = "center",
-      tabs_layout = "equal",
-      show_separator_on_edge = false,
-    },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-      filtered_items = {
-        show_hidden_count = false,
-        hide_by_pattern = { -- uses glob style patterns
-          "__pycache__",
-        },
-      },
-    },
-    window = {
-      mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-        ["<space>"] = "none",
-        ["Y"] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-          end,
-          desc = "copy path to clipboard",
-        },
-        ["O"] = {
-          function(state)
-            require("lazy.util").open(state.tree:get_node().path, { system = true })
-          end,
-          desc = "Open with System Application",
-        },
-        ["P"] = { "toggle_preview", config = { use_float = true } },
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-      icon = {
-        folder_closed = "",
-        folder_open = "",
-        folder_empty = "",
-        folder_empty_open = "",
-      },
-      modified = { symbol = "" },
-      git_status = { symbols = icons.git },
-      diagnostics = { symbols = icons.diagnostics },
-    },
-  },
-  config = function(_, opts)
-    local function on_move(data)
-      Snacks.rename.on_rename_file(data.source, data.destination)
-    end
-
-    local events = require("neo-tree.events")
-    opts.event_handlers = opts.event_handlers or {}
-    vim.list_extend(opts.event_handlers, {
-      { event = events.FILE_MOVED, handler = on_move },
-      { event = events.FILE_RENAMED, handler = on_move },
-    })
-
-    require("neo-tree").setup(opts)
-    vim.api.nvim_create_autocmd("TermClose", {
-      pattern = "*lazygit",
-      callback = function()
-        if package.loaded["neo-tree.sources.git_status"] then
-          require("neo-tree.sources.git_status").refresh()
-        end
-      end,
-    })
-  end,
 }
