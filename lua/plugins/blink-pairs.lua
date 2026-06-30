445f661628de044df5ca7d3d0eec228fca361535
yxuefeng <yxuefeng@infervision.com>
2026-06-30 15:36:37 +0800

    fix: blink.pairs pre-download error

--- lua/plugins/blink-pairs.lua
+++ lua/plugins/blink-pairs.lua
@@ -5,10 +5,11 @@ return {
   version = "*",
   -- download prebuilt binaries from github releases, must be on a versioned release
   build = function()
-  require("blink.pairs").download():pwait(60000)
+    require("blink.pairs").download():pwait(60000)
   end,
   -- OR build from source
   -- build = function() require('blink.pairs').build():pwait(60000) end,
+  event = "TurboLoad",
 
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
